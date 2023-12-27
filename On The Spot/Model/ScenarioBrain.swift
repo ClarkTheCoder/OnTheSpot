c914df74ca15429c32b912d9ba51b846f7561291
Carson Clark <carsonclark@mbp14.local>
2023-12-27 00:04:12 -0800

    create scenario data

---
+++ On The Spot/Model/ScenarioBrain.swift
@@ -0,0 +1,16 @@
+//
+//  ScenarioBrain.swift
+//  On The Spot
+//
+//  Created by Carson Clark on 2023-12-26.
+//
+
+import Foundation
+
+struct ScenarioBrain {
+    let scenarioArray =  [
+        Scenario(question: "You see a coworker's significant other on a mobile dating app, do you notify your colleague?"),
+        Scenario(question: "You see a coworker's significant other on a mobile dating app, do you notify your colleague?"),
+        Scenario(question: "You see a coworker's significant other on a mobile dating app, do you notify your colleague?")
+    ]
+}
