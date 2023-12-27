c0fdd6057f09b377f0bc582949b32fd1b9d75f5f
Carson Clark <carsonclark@mbp14.local>
2023-12-27 00:01:16 -0800

    create viewmodel to manage logic relating to scenarios

--- On The Spot/ViewModel/ScenarioViewModel.swift
+++ On The Spot/ViewModel/ScenarioViewModel.swift
@@ -9,5 +9,23 @@ import Foundation
 import SwiftUI
 
 class ScenarioViewModel: ObservableObject {
+    @Published private var scenarios: [Scenario]
+    @Published var currentScenarioIndex: Int = 0
+
+    // intializde with array of scenario items
+    init(scenarios: [Scenario]) {
+        self.scenarios = scenarios
+    }
     
+    var currentScenario: Scenario {
+        scenarios[currentScenarioIndex]
+    }
+    
+    func nextScenario() {
+        if currentScenarioIndex < scenarios.count - 1 {
+            currentScenarioIndex += 1
+        } else {
+            currentScenarioIndex = 0
+        }
+    }
 }
